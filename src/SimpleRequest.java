2a912875d5e309ada5317cbb259ad496e0fc9b90
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-02-16 17:49:27 +0100

    Update SimpleRequest.java

---
+++ src/SimpleRequest.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+// 
+public class SimpleRequest extends Request {
+
+    // super
+    public SimpleRequest(String day, String departure, String arrival, String hour) throws IOException, ParseException {
+        super(day, departure, arrival, hour);
+    }
+
+    //
+
+}
