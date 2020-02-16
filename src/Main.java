d8b299e9a3311c955c065b16d763190784577536
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-02-16 17:45:07 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.io.IOException;
+import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Sibra sibra = new Sibra();
-//        sibra.test();
-        sibra.loadData();
+    public static void main(String[] args) throws IOException, ParseException {
+
+        // init request
+        String day = "2020-02-14";
+        String departure = "CAMPUS";
+        String arrival = "GARE";
+        String hour = "12:00";
+
+        Request request = new Request(day, departure, arrival, hour);
+
+        System.out.println(request.typeDay);
 
     }
 }
