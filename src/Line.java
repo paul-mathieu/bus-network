911134c6d4f85a89b321b80000c752507a78a924
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-02-16 17:45:05 +0100

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -1,23 +1,42 @@
 import java.util.ArrayList;
 
+// ex: each senses
+
 public class Line {
 
     private ArrayList<String> rawDataLine;
-    public ArrayList<BusStop> dataLine;
+
+    public ArrayList<BusStop> dataLine = new ArrayList<>();
+    public ArrayList<String> listBusStopName = new ArrayList<>();
 
     // initialisation
     public Line(ArrayList<String> rawDataLine){
         this.rawDataLine = rawDataLine;
         convertRawData();
+        setListBusStopName();
     }
 
     // print
     public void print(){
-        System.out.println(this.rawDataLine);
+        System.out.println("========================");
+//        for (BusStop busStop: this.dataLine) busStop.print();
+        System.out.println(this.listBusStopName);
+        System.out.println("========================");
     }
 
     // convert raw data
     public void convertRawData(){
-        // écrire le code pour convertir des lignes
+        for (String busStop: this.rawDataLine){
+//            System.out.println(busStop);
+            if (!busStop.equals("")) {
+                this.dataLine.add(new BusStop(busStop));
+            }
+        }
+    }
+
+    public void setListBusStopName(){
+        for (BusStop bs: this.dataLine){
+            this.listBusStopName.add(bs.getName());
+        }
     }
 }
