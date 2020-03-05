eae9ef1eaea6105242107f1e05ca03939a1bc3c4
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-05 10:31:18 +0100

    Update DijkstraRequest.java

--- src/DijkstraRequest.java
+++ src/DijkstraRequest.java
@@ -3,11 +3,10 @@ import java.text.ParseException;
 
 public class DijkstraRequest extends Request {
 
-    private Graph graph;
-
     public DijkstraRequest(String day, String departure, String arrival, String hour) throws IOException, ParseException {
         super(day, departure, arrival, hour);
     }
+
 
 
 }
