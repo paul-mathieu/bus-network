13c9a835cd4a62980ea067a1b646a2e62a0c96c8
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-06 10:01:15 +0100

    Update BusStop.java

--- src/BusStop.java
+++ src/BusStop.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 // ex: Campus
 
@@ -8,10 +9,19 @@ public class BusStop {
     public String name = "";
     public ArrayList<String> listHourOfPassage = new ArrayList<>();
 
+    public Boolean isSeparation = false;         // TODO if it's one of two possibilities
+    public Boolean isJunctionSeparation = false; // TODO if there are two possibilities next
+    public Boolean isCrossingLines = false;      // TODO if an other bus line stops there
+    public Boolean isNotAlready = false;         // if don't already pass
+    public Boolean isTerminus = false;           // TODO if is terminus (even if not already)
+
+    public boolean isUsed = false;
 
     public BusStop(String rawDataBusStop){
         this.rawDataBusStop = rawDataBusStop;
         convertRawData();
+        setIsNotAlready();
+        setIsTerminus();
     }
 
     public void print(){
@@ -23,9 +33,7 @@ public class BusStop {
 
     private void convertRawData(){
         String[] rawDataSplit = this.rawDataBusStop.split("\\s");
-
         for (String element : rawDataSplit) {
-//            System.out.println(element);
             if (!this.name.equals("")) this.listHourOfPassage.add(element);
             else if (!element.equals("")) this.name = element;
         }
@@ -36,4 +44,25 @@ public class BusStop {
         return this.name;
     }
 
+    /*
+    ==================================================
+     Setters
+    ==================================================
+     */
+
+    private void setIsNotAlready(){
+        if (this.rawDataBusStop.contains("-")){
+            this.isNotAlready = true;
+        }
+    }
+
+    public void setIsUsed(boolean b){
+        this.isUsed = b;
+    }
+
+    private void setIsTerminus(){
+        if (true){
+            this.isTerminus = true;
+        }
+    }
 }
