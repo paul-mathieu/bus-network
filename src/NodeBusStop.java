e7e19bf81dccbfab2a1801035aae1f90f10b0d47
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-08 10:48:15 +0100

    Create NodeBusStop.java

---
+++ src/NodeBusStop.java
@@ -0,0 +1,13 @@
+public class NodeBusStop {
+
+    public String nameBus;
+    public String nameLine;
+    public BusStop busStop;
+
+    public NodeBusStop(String nameBus, String nameLine, BusStop busStop){
+        this.nameBus = nameBus;
+        this.nameLine = nameLine;
+        this.busStop = busStop;
+    }
+
+}
