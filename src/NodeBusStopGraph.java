9bf67be7581152d78e3df588c2c7a7e236fab634
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-27 13:14:04 +0100

    Update NodeBusStopGraph.java

--- src/NodeBusStopGraph.java
+++ src/NodeBusStopGraph.java
@@ -2,12 +2,36 @@ import java.util.ArrayList;
 
 public class NodeBusStopGraph extends NodeBusStop {
 
+    /*
+    =======================================================================
+     Initialization
+    =======================================================================
+    */
+
     private int timeToNextStep = -1;
     private ArrayList<NodeBusStopGraph> nodeBusStopChildren = new ArrayList<>();
 
     public NodeBusStopGraph(String nameBus, String nameLine, BusStop busStop) {
         super(nameBus, nameLine, busStop);
     }
+
+
+    /*
+    =======================================================================
+     Setters
+    =======================================================================
+    */
+
+    public void setTimeToNextStep(int timeToNextStep) {
+        this.timeToNextStep = timeToNextStep;
+    }
+
+
+    /*
+    =======================================================================
+     Getters
+    =======================================================================
+    */
 
     public int getTimeToNextStep() {
         return this.timeToNextStep;
@@ -17,9 +41,12 @@ public class NodeBusStopGraph extends NodeBusStop {
         return nodeBusStopChildren;
     }
 
-    public void setTimeToNextStep(int timeToNextStep) {
-        this.timeToNextStep = timeToNextStep;
-    }
+
+    /*
+    =======================================================================
+     Methods
+    =======================================================================
+    */
 
     public void addNodeBusStopChild(NodeBusStopGraph nodeBusStopChild){
         this.nodeBusStopChildren.add(nodeBusStopChild);
