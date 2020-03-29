37a716cdba37f6e0c00f97d6a026cc2c349d5021
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-29 15:21:35 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,21 @@ public class Main {
         String day = "2020-02-14";
         String departure = "Ponchy";
         String arrival = "POISY_COLLÈGE";
+//        String arrival = "CAMPUS";
         String hour = "12:00";
 
         DijkstraRequestShortest dijkstraRequestShortest = new DijkstraRequestShortest(day, departure, arrival, hour);
 //        System.out.println(dijkstraRequestShortest.getTimeBetweenTwoBusStop("VIGNIÈRES", "CAMPUS"));
+//        System.out.println(dijkstraRequestShortest.getTimeBetweenTwoBusStop("CAMPUS", "VIGNIÈRES"));
 //        System.out.println(dijkstraRequestShortest.getInfoLine("VIGNIÈRES", "CAMPUS"));
 //        System.out.println(dijkstraRequestShortest.getInfoLine("GARE", "Courier"));
 //        System.out.println(dijkstraRequestShortest.getInfoLine("GARE", "Bonlieu"));
 
         dijkstraRequestShortest.doRequest();
 //        System.out.println(dijkstraRequestShortest.typeDay);
+
+//        System.out.println(hour);
+//        System.out.println(hour.in);
 
     }
 
