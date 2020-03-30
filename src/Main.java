911b86187abecd32270c185125601d19f7f798d6
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-30 10:46:53 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,20 @@ public class Main {
         // init request
         String day = "2020-02-14";
         String departure = "Ponchy";
-        String arrival = "POISY_COLLÈGE";
-//        String arrival = "CAMPUS";
+//        String arrival = "POISY_COLLÈGE";
+        String arrival = "CAMPUS";
         String hour = "12:00";
 
-        DijkstraRequestShortest dijkstraRequestShortest = new DijkstraRequestShortest(day, departure, arrival, hour);
+//        DijkstraRequestShortest dijkstraRequestShortest = new DijkstraRequestShortest(day, departure, arrival, hour);
 //        System.out.println(dijkstraRequestShortest.getTimeBetweenTwoBusStop("VIGNIÈRES", "CAMPUS"));
-//        System.out.println(dijkstraRequestShortest.getTimeBetweenTwoBusStop("CAMPUS", "VIGNIÈRES"));
-//        System.out.println(dijkstraRequestShortest.getInfoLine("VIGNIÈRES", "CAMPUS"));
-//        System.out.println(dijkstraRequestShortest.getInfoLine("GARE", "Courier"));
-//        System.out.println(dijkstraRequestShortest.getInfoLine("GARE", "Bonlieu"));
-
-        dijkstraRequestShortest.doRequest();
 //        System.out.println(dijkstraRequestShortest.typeDay);
+//        dijkstraRequestShortest.doRequest();
 
-//        System.out.println(hour);
-//        System.out.println(hour.in);
+//        DijkstraRequestForemost dijkstraRequestForemost = new DijkstraRequestForemost(day, departure, arrival, hour);
+//        dijkstraRequestForemost.doRequest();
+
+        DijkstraRequestFastest dijkstraRequestFastest = new DijkstraRequestFastest(day, departure, arrival, hour);
+        dijkstraRequestFastest.doRequest();
 
     }
 
